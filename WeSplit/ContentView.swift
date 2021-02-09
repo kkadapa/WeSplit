0ba2788fdde8acdc6dea8cdade1ba57268a890a5
kkadapa <karthikkadapa88@gmail.com>
2021-02-09 16:15:06 -0500

    learned Forms, Groups and Sections

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,8 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section {
+                Text("Hello World")
+                Text("Hello World")
+                Text("Hello World")
+            }
+            Section{
+                Text("Hello World")
+                Text("Hello World")
+                Text("Hello World")
+            }
+            
+            }
     }
 }
 
