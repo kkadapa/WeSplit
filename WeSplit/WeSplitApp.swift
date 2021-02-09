56f99766526957ec5e9b7b2100cf4c16dda72839
kkadapa <karthikkadapa88@gmail.com>
2021-02-08 23:23:35 -0500

    initial swift ui app

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by BruceWayne on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
